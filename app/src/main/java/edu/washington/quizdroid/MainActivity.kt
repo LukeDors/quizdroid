b2a5eb7d17c18317ea91c87238276296c733971c
LukeDors <ldorsett@uw.edu>
2022-02-03 01:51:46 -0600

    Mostly works

--- app/src/main/java/edu/washington/quizdroid/MainActivity.kt
+++ app/src/main/java/edu/washington/quizdroid/MainActivity.kt
@@ -1,58 +1,33 @@
 package edu.washington.quizdroid
 
-import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import android.view.Menu
-import android.view.MenuItem
-import edu.washington.quizdroid.databinding.ActivityMainBinding
+import android.os.Bundle
+import android.content.Intent
+import android.view.View
+import android.widget.Button
+
+const val OVERVIEW_TITLE = "Topic"
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var appBarConfiguration: AppBarConfiguration
-    private lateinit var binding: ActivityMainBinding
+    lateinit var physics : Button
+    lateinit var math : Button
+    lateinit var marvel : Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        physics = findViewById<Button>(R.id.physics)
+        math = findViewById<Button>(R.id.math)
+        marvel = findViewById<Button>(R.id.marvel)
+    }
 
-        setSupportActionBar(binding.toolbar)
-
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, appBarConfiguration)
-
-        binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+    fun overview(view: View) {
+        val btn = view as Button
+        val txt = btn.getText().toString()
+        val intent = Intent(this, QuestionPage::class.java).apply {
+            putExtra(OVERVIEW_TITLE, txt)
         }
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
-    }
-
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        return navController.navigateUp(appBarConfiguration)
-                || super.onSupportNavigateUp()
+        startActivity(intent)
     }
 }
---
+++ app/src/main/java/edu/washington/quizdroid/QuestionPage.kt
@@ -0,0 +1,41 @@
+package edu.washington.quizdroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+
+const val QUIZ_TITLE = "Title"
+
+class QuestionPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_question_page)
+
+        val title = intent.getStringExtra(OVERVIEW_TITLE)
+        val subtitle = findViewById<TextView>(R.id.subtitle).apply {
+            text = title
+        }
+        val overview = findViewById<TextView>(R.id.overview)
+        if (title == "Physics") {
+            overview.text = getString(R.string.physicsOverview)
+        } else if (title == "Math") {
+            overview.text = getString(R.string.mathOverview)
+        } else if (title == "Marvel Superheroes") {
+            overview.text = getString(R.string.marvelOverview)
+        }
+    }
+
+    fun home(view: View) {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+    }
+
+    fun startQuiz(view: View) {
+        val intent = Intent(this, Quiz::class.java).apply {
+            putExtra(QUIZ_TITLE, title)
+        }
+        startActivity(intent)
+    }
+}
